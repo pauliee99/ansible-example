b76a466dc282cd01946d5f6f0fc50f1726895e28
it21871-dev <it21871@hua.gr>
2021-06-05 12:39:04 +0300

    shell script added

---
+++ define.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+echo $'---\ndeploymentservers:\n  hosts:'
+TARGET1="20.93.162.152"
+if ssh 'it21871@'$TARGET1 true; then
+  echo $'    devops1:\n      ansible_host: '$TARGET1$'\n      ansible_port: 22\n      ansible_ssh_user: it21871'
+fi
+TARGET2="13.81.28.109"
+if ssh 'azureuser@'$TARGET2 true; then
+  echo $'    deployment1:\n      ansible_host: '$TARGET2$'\n      ansible_port: 22\n      ansible_ssh_user: azureuser'
+fi
+echo $'  vars:\n    ansible_python_interpreter: /usr/bin/python3'
