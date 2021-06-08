fd36b770b2d6bf132de56ad2d71ae0153d391ce9
it21871-dev <it21871@hua.gr>
2021-06-08 17:33:49 +0300

    group vars from shell script

---
+++ define_user_dir.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+START='user_dir: "/home/'
+
+USER1="it21871"
+TARGET1="20.93.162.152"
+USER2="azureuser"
+TARGET2="13.81.28.109"
+if ssh $USER1'@'$TARGET1 true; then
+  echo $START$USER1'"'
+elif ssh $USER2'@'$TARGET2 true; then
+  echo $USER2'"'
+else
+  echo $START'"'
+fi
+
+value=`cat text.txt`
+echo "$value"
