e99114eaba2fd93f0f0cc8972d6380ef75190f8c
it21871-dev <it21871@hua.gr>
2021-06-17 15:46:01 +0300

    docker is about to finish

--- define_docker.sh
+++ define_docker.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 echo $'---\ndeploymentservers:\n  hosts:'
-TARGET1="20.56.26.193"
+TARGET1="13.69.56.28"
 if ssh 'it21871@'$TARGET1 true; then
-  echo $'    devopsvm:\n      ansible_host: '$TARGET1$'\n      ansible_port: 22\n      ansible_ssh_user: it21871'
+  echo $'    deployment1:\n      ansible_host: '$TARGET1$'\n      ansible_port: 22\n      ansible_ssh_user: it21871'
 fi
 TARGET2="13.81.28.109"
 if ssh 'azureuser@'$TARGET2 true; then
